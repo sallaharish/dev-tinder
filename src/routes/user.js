968b5af82577d99c65cc917bc57c9f308e9181e8
Salla Harish <sallaharish040@gmail.com>
2025-06-25 17:06:04 +0530

    feed api changes

--- src/routes/user.js
+++ src/routes/user.js
@@ -9,7 +9,7 @@ const jwt = require('jsonwebtoken'); // Importing JWT for authentication
 
 const connection = require("../config/database"); // Importing the database connection
 const connectionRequest = require('../models/connectionRequest'); // Importing the connection request model
-const { model } = require('mongoose');
+const { model, set } = require('mongoose');
 
 
 userRouter.get("/user/request/received", auth, async (req, res) => {
@@ -43,12 +43,55 @@ userRouter.get("/user/connections", auth, async (req, res) => {
 
                 // requests where the user is the recipient 
             ]
-        }).populate('fromUserId', ["firstName" ,"lastName"])
-        .populate('toUserId', ["firstName" ,"lastName"]); // Populate both fromUserId and toUserId fields with user details
+        }).populate('fromUserId', ["firstName" ,"lastName"]).populate('toUserId', ["firstName" ,"lastName"]); // Populate both fromUserId and toUserId fields with user details
+        // Populate the fromUserId and toUserId fields with user details
+       const data = connections.map((row)=> {
+           if(row.fromUserId._id.toString() === userId){
+            return row.toUserId
+              } 
+              else{
+                return row.fromUserId
+              }
+
+        });
         console.log("Connections:", connections); // Log the connections for debugging
-        res.status(200).json({message:"data fetched successfully" , data:connections});
+        res.status(200).json({message:"data fetched successfully" , data:data});
     } catch (error) {
         console.error('Error fetching connections:', error);
+        res.status(500).send('Internal Server Error'); // Handle errors appropriately
+    }   
+});
+userRouter.get("/feed", auth, async (req, res) => {
+    try {
+        const userId = req.user.userId; 
+        // Assuming the user ID is stored in the token
+        const allConnections = await connectionRequest.find({
+            $or: [
+                { fromUserId: userId}, // Find requests where the user is the sender
+                { toUserId: userId} // Find requests where the user is the recipient
+            ]
+        }).select('fromUserId toUserId'); // Select only the necessary fields
+    
+
+        const hideUsersFromFeed = new Set(); // Initialize a set to store user IDs to hide from the feed
+
+        allConnections.forEach((connection) => {
+            hideUsersFromFeed.add(connection.fromUserId)
+            hideUsersFromFeed.add(connection.toUserId);
+        });
+
+        const feedData = await User.find({
+             // Exclude users in the hideUsersFromFeed set
+             $or:[
+                { _id: { $nin: Array.from(hideUsersFromFeed) } },
+                { _id: userId } // Include the current user in the feed
+             ]
+        }).select('firstName lastName')
+       
+        console.log("Feed data:", data); // Log the feed data for debugging
+        res.status(200).json({message:"data fetched successfully" , data:feedData}); // Return feed data
+    } catch (error) {
+        console.error('Error fetching feed:', error);
         res.status(500).send('Internal Server Error'); // Handle errors appropriately
     }   
 });
